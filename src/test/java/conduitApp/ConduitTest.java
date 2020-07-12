dfed39ae2a4a624f8a0878f22813ff2d6f63eff4
artembondar <artem.bondar@aa.com>
2020-07-12 17:16:04 -0500

    runner config

--- src/test/java/conduitApp/ConduitTest.java
+++ src/test/java/conduitApp/ConduitTest.java
@@ -1,7 +1,9 @@
 package conduitApp;
 
+import com.intuit.karate.KarateOptions;
 import com.intuit.karate.junit5.Karate;
 
+@KarateOptions( tags = {"@debug", "@regression"})
 class ConduitTest {
     
     // this will run all *.feature files that exist in sub-directories
