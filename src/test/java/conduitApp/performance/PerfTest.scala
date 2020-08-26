d652f82169bdf4c723f247062616e98a5729d99c
artembondar <artem.bondar@aa.com>
2020-08-25 20:28:50 -0500

    simulation flow

--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -16,7 +16,13 @@ class PerfTest extends Simulation {
 
   setUp(
     createArticle.inject(
-        atOnceUsers(3)
+          atOnceUsers(1),
+          nothingFor(4 seconds),
+          constantUsersPerSec(1) during (10 seconds),
+          constantUsersPerSec(2) during (10 seconds),
+          rampUsersPerSec(2) to 10 during (20 seconds),
+          nothingFor(5 seconds),
+          constantUsersPerSec(1) during (5 seconds)
         ).protocols(protocol)
   )
 
