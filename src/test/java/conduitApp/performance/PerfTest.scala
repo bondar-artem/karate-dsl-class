9eebcf8f7553bdc523245e3589e1d7ffc4800b27
artembondar <artem.bondar@aa.com>
2020-08-30 13:41:15 -0500

    dispatcher config

--- src/test/java/conduitApp/performance/PerfTest.scala
+++ src/test/java/conduitApp/performance/PerfTest.scala
@@ -29,10 +29,10 @@ class PerfTest extends Simulation {
           atOnceUsers(1),
           nothingFor(4 seconds),
           constantUsersPerSec(1) during (3 seconds),
-          // constantUsersPerSec(2) during (10 seconds),
-          // rampUsersPerSec(2) to 10 during (20 seconds),
-          // nothingFor(5 seconds),
-          // constantUsersPerSec(1) during (5 seconds)
+          constantUsersPerSec(2) during (10 seconds),
+          rampUsersPerSec(2) to 10 during (20 seconds),
+          nothingFor(5 seconds),
+          constantUsersPerSec(1) during (5 seconds)
         ).protocols(protocol)
   )
 
